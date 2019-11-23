866e329a720f24a90d1da60f75bd40bb2c791da7
Misha Zhukov <3v1l91l@gmail.com>
2019-11-23 16:23:09 +0200

    Add generation of _id to the lessons sub document

--- src/api/common/course/courseService.js
+++ src/api/common/course/courseService.js
@@ -1,3 +1,4 @@
+const { ObjectID } = require('mongodb');
 const CourseRepository = require('./courseRepository');
 
 class CourseService {
@@ -26,6 +27,12 @@ class CourseService {
   updateCourse(course) {
     const id = course._id;
     delete course._id;
+    course.lessons.forEach(lesson => {
+      if (!lesson._id) {
+        lesson._id = new ObjectID();
+      }
+    });
+    
     return Promise.all([
       this.repository.edit(id, course),
     ])
