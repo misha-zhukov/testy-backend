c151da84cf3697981c668836cd99d13cc75075e8
Misha Zhukov <3v1l91l@gmail.com>
2019-11-27 17:47:35 +0200

    Add file upload

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const config = require('config');
 const logger = require('./utils/logger');
+var multer = require('multer');
 
 require('./passport');
 
@@ -23,6 +24,11 @@ const SeedService = require('./api/seedService');
 const seedService = new SeedService();
 
 const app = express();
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 const { port, root } = config.get('api');
 
 function logErrors(err, req, res, next) {
@@ -37,6 +43,33 @@ function clientErrorHandler(err, req, res, next) {
     next(err);
   }
 }
+const DIR = './uploads';
+
+let storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, DIR);
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + '.' + path.extname(file.originalname));
+  }
+});
+let upload = multer({storage: storage});
+
+app.post('/api/upload',upload.single('photo'), function (req, res) {
+  if (!req.file) {
+      console.log("No file received");
+      return res.send({
+        success: false
+      });
+  
+    } else {
+      console.log('file received successfully');
+      return res.send({
+        success: true
+      })
+    }
+});
+
 
 app.use(cors());
 app.use(bodyParser.json());
